da28a50e7fca1add7ee56410e51605a85c5c4a72
Rodey Luo <rodeyluo@gmail.com>
2017-07-06 16:13:31 +0800

    strict mode

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 /**
  * Created by Rodey on 2017/4/7.
  */
+'use strict';
 
-var fs          = require('fs'),
+const
+    fs          = require('fs'),
     util        = require('util'),
     concatCSS   = require("gulp-concat-css"),
     PluginError = require('gulp-util').PluginError;
 
-var PLUGIN_NAME = 'gulp-merge-css';
+const PLUGIN_NAME = 'gulp-merge-css';
 
 
-var mergeCSS = function(options){
-    var opts = util._extend({}, options || {});
-    var destFile = opts['name'] || opts['fileName'] || 'app.css';
+let mergeCSS = function(options){
+    let opts = util._extend({}, options || {});
+    let destFile = opts['name'] || opts['fileName'] || 'app.css';
 
-    var stream;
+    let stream;
     try{
         stream = concatCSS(destFile, options);
     }catch(e){
